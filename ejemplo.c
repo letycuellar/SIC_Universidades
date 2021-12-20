7ab7a6017c7406e2bbbbc74803430665c3d389b4
letycuellar <96089255+letycuellar@users.noreply.github.com>
2021-12-20 17:42:00 -0600

    Create ejemplo.c

---
+++ ejemplo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float A,B,C;
+	printf("ingrese el primer numero\n");
+	scanf("%f",&A);
+	printf("ingrese el segundo numero\n");
+	scanf("%f",&B);
+	C=A+B;
+	printf("El resultado es: %f",C);
+	return 0;
+}
